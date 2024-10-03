d933f791a378c0291e34bf7ad6cf4551b423806a
Noskina Olha <noskinaolga1@gmail.com>
2024-10-03 18:43:17 +0200

    Prevent default form submission behavior in NewsletterBox component

--- src/components/NewsletterBox.jsx
+++ src/components/NewsletterBox.jsx
@@ -1,20 +1,31 @@
 const NewsletterBox = () => {
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+  };
   return (
     <div className="text-center">
       <p className="textt-2xl font-medium text-gray-800">
         Subscribe now & get 20% off
       </p>
       <p className="mt-3 text-gray-400">
-      Subscribe to our newsletter and get 20% off your first order!
+        Subscribe to our newsletter and get 20% off your first order!
       </p>
-      <form className="w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 pl-3 border">
+      <form
+        onSubmit={onSubmitHandler}
+        className="w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 pl-3 border"
+      >
         <input
           type="email"
           placeholder="Enter Your Email"
           required
           className="w-full sm:flex-1 outline-none"
         />
-        <button className="bg-black text-white text-xs px-10 py-4" type="submit">SUBSCRIBE</button>
+        <button
+          className="bg-black text-white text-xs px-10 py-4"
+          type="submit"
+        >
+          SUBSCRIBE
+        </button>
       </form>
     </div>
   );
