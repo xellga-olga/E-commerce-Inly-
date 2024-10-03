9ad2f5b2b9c4fc4e9f90adb167544ad124c912ce
Noskina Olha <noskinaolga1@gmail.com>
2024-10-03 18:12:13 +0200

    Add NewsletterBox and OurPolice components

---
+++ src/components/NewsletterBox.jsx
@@ -0,0 +1,23 @@
+const NewsletterBox = () => {
+  return (
+    <div className="text-center">
+      <p className="textt-2xl font-medium text-gray-800">
+        Subscribe now & get 20% off
+      </p>
+      <p className="mt-3 text-gray-400">
+      Subscribe to our newsletter and get 20% off your first order!
+      </p>
+      <form className="w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 pl-3 border">
+        <input
+          type="email"
+          placeholder="Enter Your Email"
+          required
+          className="w-full sm:flex-1 outline-none"
+        />
+        <button className="bg-black text-white text-xs px-10 py-4" type="submit">SUBSCRIBE</button>
+      </form>
+    </div>
+  );
+};
+
+export default NewsletterBox;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import BestSellers from "../components/BestSellers"
 import Hero from "../components/Hero"
 import LatestCollections from "../components/LatestCollections"
+import NewsletterBox from "../components/NewsletterBox"
+import OurPolice from "../components/OurPolice"
 
 const Home = () => {
   return (
@@ -8,6 +10,8 @@ const Home = () => {
       <Hero />
       <LatestCollections />
       <BestSellers />
+      <OurPolice />
+      <NewsletterBox />
     </div>
   )
 }
