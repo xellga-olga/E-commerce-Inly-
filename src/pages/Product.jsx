1ef726553b36926753b9bed5939f0d100ae07eee
Noskina Olha <noskinaolga1@gmail.com>
2024-10-08 18:35:56 +0200

    Added product page with size selection, image gallery, and rating display

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,11 +1,97 @@
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
 
+import star_icon from "../assets/star_icon.svg";
+import star_dull_icon from "../assets/star_dull_icon.svg";
 
 const Product = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { productId } = useParams();
+  const { products, currency } = useContext(ShopContext);
+  const [productData, setProductData] = useState(false);
+  const [image, setImage] = useState("");
+  const [size, setSize] = useState("");
 
-export default Product
+  const fetchProductData = async () => {
+    products.map((item) => {
+      if (item.id === productId) {
+        setProductData(item);
+        setImage(item.image[0]);
+        return null;
+      }
+    });
+  };
+
+  useEffect(() => {
+    fetchProductData();
+  }, [productId]);
+
+  return productData ? (
+    <div className="border-t2 pt-10 transition-opacity ease-in duration-500 opacity-100">
+      {/* Product Data */}
+      <div className="flex gap-12 sm:gap-12 flex-col sm:flex-row">
+        {/* Product Images */}
+        <div className="flex-1 flex flex-col-reverse gap-3 sm:flex-row">
+          <div className="flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full">
+            {productData.image.map((item, index) => (
+              <img
+                onClick={() => setImage(item)}
+                src={item}
+                key={index}
+                className="cursor-pointer w-[24% sm:w-full sm:mb-3 flex-shrink-0]"
+              />
+            ))}
+          </div>
+          <div className="w-full sm:w-[80%]">
+            <img src={image} className="w-full h-auto" />
+          </div>
+        </div>
+
+        {/* Product Info */}
+        <div className="flex-1">
+          <h1 className="font-medium text-2xl mt-2">{productData.name}</h1>
+          <div className="flex items-center gap-1 mt-2">
+            <img src={star_icon} className="w-3" />
+            <img src={star_icon} className="w-3" />
+            <img src={star_icon} className="w-3" />
+            <img src={star_icon} className="w-3" />
+            <img src={star_dull_icon} className="w-3" />
+            <p className="pl-2">{122}</p>
+          </div>
+          <p className="mt-5 font-medium text-3xl">
+            {currency}
+            {productData.price}
+          </p>
+          <p className="mt-5 text-gray-500 md:w-4/5">
+            {productData.description}
+          </p>
+          <div className="flex flex-col my-8 gap-4">
+            <p>Select Size</p>
+            <div className="flex gap-2">
+              {productData.sizes.map((item, index) => (
+                <button
+                  onClick={() => setSize(item)}
+                  className={`${item === size ? 'border-orange-500' : '' } border bg-gray-100 py-2 px-4`}
+                  key={index}
+                >
+                  {item}
+                </button>
+              ))}
+            </div>
+          </div>
+          <button className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700">ADD TO CART</button>
+          <hr className="mt-8 sm:w-4/5" />
+          <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1">
+            <p>100% Original product.</p>
+            <p>Cash on delivery is available on this product.</p>
+            <p>Easy return and exchange policy within 7 days.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div className="opacity-0"></div>
+  );
+};
+
+export default Product;
