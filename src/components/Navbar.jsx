a1b0251d52dc864eb790b784ae59746aaa6ee551
Noskina Olha <noskinaolga1@gmail.com>
2024-10-09 18:44:24 +0200

    create add to cart feature and added toast notification for prompting user to select product size using react-toastify

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ import { ShopContext } from "../context/ShopContext";
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
 
-  const { setShowSearch } = useContext(ShopContext)
+  const { setShowSearch, getCartCount } = useContext(ShopContext)
 
   return (
     <div className="flex items-center justify-between py-5 font-medium">
@@ -61,7 +61,7 @@ const Navbar = () => {
             alt="cart"
           />
           <p className="absolute right-[-5px] bottom-[-5px] w-4  text-center leading-4 bg-black text-white text-[8px] rounded-full aspect-square">
-            10
+           {getCartCount()}
           </p>
         </Link>
         <img
--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,26 +1,73 @@
-import { createContext, useState } from "react"
-import { products } from "../assets/products"
+import { createContext, useEffect, useState } from "react";
+import { products } from "../assets/products";
+import { toast } from "react-toastify";
 
-
-export const ShopContext = createContext()
+export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
-
    const currency = "$";
    const delivery_fee = 10;
-   const[search, setSearch] = useState('')
-   const[showSearch, setShowSearch] = useState(false)
+   const [search, setSearch] = useState("");
+   const [showSearch, setShowSearch] = useState(false);
+   const [cartItems, setCartItems] = useState({});
+
+   const getCartCount = () => {
+      let totalCount = 0;
+      for (const items in cartItems) {
+         for (const item in cartItems[items]) {
+            try {
+               if (cartItems[items][item] > 0) {
+                  totalCount += cartItems[items][item];
+               }
+            } catch (error) {
+               console.error(error);
+            }
+         }
+      }
+      return totalCount;
+   };
+
+   const addToCart = async (itemId, size) => {
+      if (!size) {
+         toast.error("Select Product Size");
+         return;
+      }
+
+      let cartData = structuredClone(cartItems);
+
+      if (cartData[itemId]) {
+         if (cartData[itemId][size]) {
+            cartData[itemId][size] += 1;
+         } else {
+            cartData[itemId][size] = 1;
+         }
+      } else {
+         cartData[itemId] = {};
+         cartData[itemId][size] = 1;
+      }
+      setCartItems(cartData);
+   };
+
+   useEffect(() => {
+      console.log(cartItems);
+   }, [cartItems]);
 
    const value = {
-      products, currency, delivery_fee,
-      search, setSearch, showSearch, setShowSearch
-   }
+      products,
+      currency,
+      delivery_fee,
+      search,
+      setSearch,
+      showSearch,
+      setShowSearch,
+      cartItems,
+      addToCart,
+      getCartCount,
+   };
 
    return (
-      <ShopContext.Provider value={value}>
-         {props.children}
-      </ShopContext.Provider>
-   )
-}
+      <ShopContext.Provider value={value}>{props.children}</ShopContext.Provider>
+   );
+};
 
-export default ShopContextProvider
+export default ShopContextProvider;
