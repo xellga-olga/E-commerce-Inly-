7fb07e16a8210647b1c4b90894899b9f9805e571
Noskina Olha <noskinaolga1@gmail.com>
2024-10-11 10:19:39 +0200

    Implemented PlaceOrder page with delivery info form and payment method selection

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/products";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 export const ShopContext = createContext();
 
@@ -10,6 +11,7 @@ const ShopContextProvider = (props) => {
   const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(false);
   const [cartItems, setCartItems] = useState({});
+  const navigate = useNavigate()
 
   const getCartCount = () => {
     let totalCount = 0;
@@ -90,6 +92,7 @@ const ShopContextProvider = (props) => {
     getCartCount,
     updateQuantity,
     getCartAmount,
+    navigate,
   };
 
   return (
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -5,7 +5,7 @@ import Bin from "../assets/bin.svg";
 import CartTotal from "../components/CartTotal";
 
 const Cart = () => {
-  const { products, currency, cartItems, updateQuantity } =
+  const { products, currency, cartItems, updateQuantity, navigate } =
     useContext(ShopContext);
 
   const [cartData, setCartData] = useState([]);
@@ -89,7 +89,7 @@ const Cart = () => {
         <div className="w-full sm:w-[450px]">
           <CartTotal />
           <div className="w-full text-end">
-            <button className="text-white bg-black text-sm my-8 px-8 py-3 ">PROCEED TO CHECKOUT</button>
+            <button onClick={() => navigate('/place-order')} className="text-white bg-black text-sm my-8 px-8 py-3 ">PROCEED TO CHECKOUT</button>
           </div> 
         </div>
       </div>
