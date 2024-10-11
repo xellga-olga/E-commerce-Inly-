2af26efed74c72c375badcaa4d30f988ebd03720
Noskina Olha <noskinaolga1@gmail.com>
2024-10-11 14:22:20 +0200

    Added Orders page with product details and order summary

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,9 +1,40 @@
+import { useContext } from "react"
+import { ShopContext } from "../context/ShopContext"
+import Title from "../components/Title"
 
 
 const Orders = () => {
+
+  const {products, currency}= useContext(ShopContext)
+
   return (
-    <div>
-      
+    <div className="border-t pt-16">
+
+      <div className="text-2xl">
+        <Title text1={'MY'} text2={'ORDERS'}/>
+      </div>
+
+      <div className="">
+        {
+          products.slice(1,4).map((item, index) => (
+            <div key={index} className="flex flex-col py-2 border-t border-b text-gray-700 md:flex-row md:items-center md:justify-between gap-4 ">
+              <div className="flex items-start gap-6 text-sm">
+                <img src={item.image[0]} className="w-20 sm:w-24"/>
+                <div>
+                  <p className="sm:text-base font-medium">{item.name}</p>
+                  <div className="flex text-base items-center gap-3 mt-2 text-gray-700">
+                    <p className="text-lg">{currency}{item.price}</p>
+                    <p>Quantity: 1</p>
+                    <p>Size: M</p>
+                  </div>
+                  <p className="mt-2">Data: <span className="text-gray-400">11, Oct, 2024</span></p>
+                </div>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+
     </div>
   )
 }
--- src/pages/PlaceOrder.jsx
+++ src/pages/PlaceOrder.jsx
@@ -1,78 +1,133 @@
-import CartTotal from "../components/CartTotal"
-import Title from "../components/Title"
-import ApplePay from '../assets/apple-pay.svg'
-import GooglePay from '../assets/google-pay.svg'
-import { useState } from "react"
-import { useNavigate } from "react-router-dom"
-import { ShopContext } from "../context/ShopContext"
+import CartTotal from "../components/CartTotal";
+import Title from "../components/Title";
+import ApplePay from "../assets/apple-pay.svg";
+import GooglePay from "../assets/google-pay.svg";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
 
 const PlaceOrder = () => {
-
-  const [method, setMethod] = useState('cod')
-  const { navigate } = useNavigate(ShopContext)
+  const [method, setMethod] = useState("cod");
+  const  navigate  = useNavigate(ShopContext);
 
   return (
     <div className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh]">
-
-
       {/* Left Side */}
       <div className="flex flex-col gap-4 w-full sm:max-w-[480px]">
         <div className="text-xl sm:text-2xl my-3">
-          <Title text1={'DELIVERY'} text2={'INFORMATION'} />
+          <Title text1={"DELIVERY"} text2={"INFORMATION"} />
         </div>
         <div className="flex gap-3">
-          <input className="border border-gray-300 rounded py-1.5 px-3.5 w-full" type="text" placeholder="First name" />
-          <input className="border border-gray-300 rounded py-1.5 px-3.5 w-full" type="text" placeholder="Last name" />
+          <input
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+            type="text"
+            placeholder="First name"
+          />
+          <input
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+            type="text"
+            placeholder="Last name"
+          />
         </div>
-        <input className="border border-gray-300 rounded py-1.5 px-3.5 w-full" type="email" placeholder="Email address" />
-        <input className="border border-gray-300 rounded py-1.5 px-3.5 w-full" type="text" placeholder="Street" />
+        <input
+          className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          type="email"
+          placeholder="Email address"
+        />
+        <input
+          className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          type="text"
+          placeholder="Street"
+        />
         <div className="flex gap-3">
-          <input className="border border-gray-300 rounded py-1.5 px-3.5 w-full" type="text" placeholder="City" />
-          <input className="border border-gray-300 rounded py-1.5 px-3.5 w-full" type="text" placeholder="State" />
+          <input
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+            type="text"
+            placeholder="City"
+          />
+          <input
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+            type="text"
+            placeholder="State"
+          />
         </div>
         <div className="flex gap-3">
-          <input className="border border-gray-300 rounded py-1.5 px-3.5 w-full" type="number" placeholder="Zipcode" />
-          <input className="border border-gray-300 rounded py-1.5 px-3.5 w-full" type="text" placeholder="Country" />
+          <input
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+            type="number"
+            placeholder="Zipcode"
+          />
+          <input
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+            type="text"
+            placeholder="Country"
+          />
         </div>
-        <input className="border border-gray-300 rounded py-1.5 px-3.5 w-full" type="numder" placeholder="Phone" />
+        <input
+          className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          type="numder"
+          placeholder="Phone"
+        />
       </div>
-
 
       {/* Right Side */}
       <div className="mt-8 min-w-80">
         <CartTotal />
         <div className="mt-12">
-          <Title text1={'PAYMENT'} text2={'METHOD'} />
+          <Title text1={"PAYMENT"} text2={"METHOD"} />
           {/* Payment Method Section */}
           <div className="flex gap-3 flex-col lg:flex-row">
-
-            <div onClick={() => setMethod("ApplePay")} className="flex items-center gap-3 p-2 border px-3 cursor-pointer">
-              <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'ApplePay' ? 'bg-green-600' : ''}`}></p>
+            <div
+              onClick={() => setMethod("ApplePay")}
+              className="flex items-center gap-3 p-2 border px-3 cursor-pointer"
+            >
+              <p
+                className={`min-w-3.5 h-3.5 border rounded-full ${
+                  method === "ApplePay" ? "bg-green-600" : ""
+                }`}
+              ></p>
               <img className="h-10 mx-4" src={ApplePay} alt="apple-pay" />
             </div>
 
-            <div onClick={() => setMethod("GooglePay")} className="flex items-center gap-3 p-2 border px-3 cursor-pointer">
-              <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'GooglePay' ? 'bg-green-600' : ''}`}></p>
+            <div
+              onClick={() => setMethod("GooglePay")}
+              className="flex items-center gap-3 p-2 border px-3 cursor-pointer"
+            >
+              <p
+                className={`min-w-3.5 h-3.5 border rounded-full ${
+                  method === "GooglePay" ? "bg-green-600" : ""
+                }`}
+              ></p>
               <img className="h-10 mx-4" src={GooglePay} alt="google-pay" />
             </div>
 
-            <div onClick={() => setMethod("cod")} className="flex items-center gap-3 p-2 border px-3 cursor-pointer">
-              <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'cod' ? 'bg-green-600' : ''}`}></p>
-              <p className="text-gray-500 text-sm font-medium mx-4">CASH ON DELIVERY</p>
+            <div
+              onClick={() => setMethod("cod")}
+              className="flex items-center gap-3 p-2 border px-3 cursor-pointer"
+            >
+              <p
+                className={`min-w-3.5 h-3.5 border rounded-full ${
+                  method === "cod" ? "bg-green-600" : ""
+                }`}
+              ></p>
+              <p className="text-gray-500 text-sm font-medium mx-4">
+                CASH ON DELIVERY
+              </p>
             </div>
-
           </div>
 
           <div className="w-full text-end mt-8">
-            <button onClick={() => navigate('/orders')} className="bg-black text-white px-16 py-3 text-sm">PLACE ORDER</button>
+            <button
+              onClick={() => navigate("/orders")}
+              className="bg-black text-white px-16 py-3 text-sm"
+            >
+              PLACE ORDER
+            </button>
           </div>
-
         </div>
       </div>
-
-
     </div>
-  )
-}
+  );
+};
 
-export default PlaceOrder
+export default PlaceOrder;
