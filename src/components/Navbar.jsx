d1abf676a9e0654239815bffd32c810b80638336
Noskina Olha <noskinaolga1@gmail.com>
2024-10-12 09:53:18 +0200

    Added Login and Sign Up form with toggle functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
       <div className="flex items-center gap-6">
         <img onClick={() => setShowSearch(true)} src={Search_Icon} className="w-5 cursor-pointer" alt="search" />
         <div className="group relative">
-          <img src={User_Icon} className="w-5 cursor-pointer" alt="user" />
+          <Link to='/login'><img src={User_Icon} className="w-5 cursor-pointer" alt="user" /></Link>
           <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
             <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
               <p className="cursor-point hover:text-black">My Profile</p>
