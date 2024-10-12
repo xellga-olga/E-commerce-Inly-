fa8647ceadfbd69d27a5b9359519c84785dff4bb
Noskina Olha <noskinaolga1@gmail.com>
2024-10-12 12:04:06 +0200

    Added Contact page content

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,7 +2,8 @@
 const Title = ({text1, text2}) => {
   return (
     <div className="inline-flex gap-2 items-center mb-3">
-      <p className="text-gray-500">{text1}
+
+      <p className="text-gray-500">{text1}   
          <span className="text-gray-700 font-medium ">{text2}</span>
       </p>
   
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,28 @@
+import Title from "../components/Title";
+import contact_img from "../assets/contact_img.jpg";
+import NewsletterBox from "../components/NewsletterBox";
 
 const Contact = () => {
   return (
     <div>
-      
-    </div>
-  )
-}
+      <div className="text-center text-2xl pt-10 border-t">
+        <Title text1={"CONTACT"} text2={"US"} />
+      </div>
 
-export default Contact
+      <div className="my-10 flex flex-row justify-center md:flex-row gap-10 mb-28">
+        <img className="w-full md:max-w-[350px]" src={contact_img} />
+        <div className="flex flex-col justify-center items-start gap-6">
+          <p className="text-xl font-semibild text-gray-600">Our Store</p>
+          <p className="text-gray-500">3532 Miskolc, Győri kapu 146</p>
+          <p className="text-gray-500">
+            Tel: (000) 000 - 1111 <br /> Email: admin_inly@test.com
+          </p>
+        </div>
+      </div>
+
+      <NewsletterBox />
+    </div>
+  );
+};
+
+export default Contact;
